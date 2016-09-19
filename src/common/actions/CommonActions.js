eef6d96ee95b23fa0fa1a75604cc2c3425b46bd5
FlyingAnt <flying2010.liu@gmail.com>
2016-09-19 11:33:29 +0800

    move the source file to the dev folder

--- src/common/actions/CommonActions.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {createActions, PureAction} from '../../utils/ActionBuilder'
-
-module.exports = createActions({
-
-    Busy: PureAction, // show spinner or not
-
-})
